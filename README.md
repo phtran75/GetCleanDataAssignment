4e20f04cb23d1a9bcbb0c11c6d67d2a07ec9f46d
Philippe Tran <philippetran.mooc@gmail.com>
2015-10-25 22:46:54 +0100

    Second commit

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # GetCleanDataAssignment
 The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set.
 
-1. Merges the training and the test sets to create one data set.
+The script uses "dplyr", which is very useful.
+
+1. Merge the training and the test sets to create one data set.
   The script uses rbind() to build columns of subjects, activities and measurements
-  then cbind() to build the first dataset
+  then cbind() to build the first dataset.
+  I chose to rename the first two columns containing a name "V1" before merging to avoid problems.
+  
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+  I counted 66 columns concerned by "mean" or "std".
+  
+3. Uses descriptive activity names to name the activities in the data set
+  Just make basic replacements from 1 to 6.
+  
+
+  
--- run_analysis.R
+++ run_analysis.R
@@ -12,6 +12,8 @@
 # From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
+library(dplyr)
+
 # Read all data files
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
@@ -20,10 +22,31 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 
-# Merge all data files
+# Merges the training and the test sets to create one data set.
 # 7352 train + 2947 test = 10299 lines and 563 columns
 first_column <- rbind(subject_train,subject_test)
 second_column <- rbind(Y_train,Y_test)
 other_column <- rbind(X_train,X_test)
 
+# I chose to rename the first two columns before merging
+first_column <- rename(first_column, Subject = V1)
+second_column <- rename(second_column, Activity = V1)
 first_dataset <- cbind(first_column, second_column, other_column)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+# I counted 66 columns... from V1 to V...
+tmp_dataset <- select(first_dataset,1:8,43:48,83:88,123:128,163:168,203:204,216:217,229:230,242:243,255:256,268:273,347:352,426:431,505:506,518:519,531:532,544:545)
+
+# Uses descriptive activity names to name the activities in the data set
+# 1 WALKING
+# 2 WALKING_UPSTAIRS
+# 3 WALKING_DOWNSTAIRS
+# 4 SITTING
+# 5 STANDING
+# 6 LAYING
+tmp_dataset$Activity[tmp_dataset$Activity == 1] <- "WALKING"
+tmp_dataset$Activity[tmp_dataset$Activity == 2] <- "WALKING_UPSTAIRS"
+tmp_dataset$Activity[tmp_dataset$Activity == 3] <- "WALKING_DOWNSTAIRS"
+tmp_dataset$Activity[tmp_dataset$Activity == 4] <- "SITTING"
+tmp_dataset$Activity[tmp_dataset$Activity == 5] <- "STANDING"
+tmp_dataset$Activity[tmp_dataset$Activity == 6] <- "LAYING"
